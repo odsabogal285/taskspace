92e17967a71f260dac5bdef9a2cc46d188f9c9df
Oscar Sabogal <oscarsabogal285@gmail.com>
2023-04-02 19:41:07 -0500

    feat(backend): add migrations of business rules

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name', 20);
+            $table->string('second_name', 20)->nullable();
+            $table->string('first_surname', 20);
+            $table->string('second_surname', 20)->nullable();
+            $table->string('email', 25)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
